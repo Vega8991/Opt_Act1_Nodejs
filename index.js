6ef2e3758e102aa3c19bc4327520d24073b6cd17
Vega8991 <manuelvega011204@gmail.com>
2025-10-03 21:32:19 +0200

    dependences added

---
+++ index.js
@@ -0,0 +1,3 @@
+import chalk from 'chalk';         // Para colorear texto en consola
+import fetch from 'node-fetch';    // Para hacer peticiones HTTP
+import moment from 'moment';       // Para gestionar fechas
