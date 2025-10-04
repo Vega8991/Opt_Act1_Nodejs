e528ce009e4a685acb9fb0856b5c9d4c77d06a6c
Vega8991 <manuelvega011204@gmail.com>
2025-10-04 14:00:10 +0200

    fechaActual, alt, lat and welcome message added

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 import chalk from 'chalk';         // Para colorear texto en consola
 import fetch from 'node-fetch';    // Para hacer peticiones HTTP
 import moment from 'moment';       // Para gestionar fechas
+
+// Mensaje de bienvenida de color verde
+console.log(chalk.green('Bienvenidos a Weather Finder :)'))
+
+// Guardamos la fecha y hora actual en una constante
+const fechaActual = moment().format('LLLL')
+
+// Imprimimos por consola la fecha y hora de colores con chalk
+console.log(chalk.blue('Fecha actual: ') + chalk.yellow(fechaActual));
+
+// Coordenadas de sevilla
+const alt = 37.38283;
+const lat = -5.97317;
+
